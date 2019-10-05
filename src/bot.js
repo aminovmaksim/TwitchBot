3e95f2f354601c308ddfa369f3d90821ca46999c
Max Aminov <aminovmaksim@gmail.com>
2019-10-05 20:36:29 +0300

    Change welcome messages

--- src/bot.js
+++ src/bot.js
@@ -1,33 +1,86 @@
 import client from "./client";
 import { resolve } from "./commandResolver";
+const axios = require('axios');
+
 client.connect();
+
+// viewers will be dropped every 24 hours
+var viewers;
+init_viewers();
+setInterval(function() {
+  viewers = []
+}, 86400000);
+
+setInterval(function() {
+  JSON.stringify(viewers)
+}, 10000);
+
 
 // Commands
 client.on("chat", (channel, user, message, self) => {
-  if (self) return; // bot message
+  if (user.username === "zortan3301") { // bot message
+    return;
+  }
+
+  if (!containsObject(user.username, viewers)) {
+    viewers.push(user.username);
+    client.action(channel, user.username + hellos[Math.floor(Math.random() * hellos.length)]);
+  }
 
   // if message has symbol whats mean command - !
   if (message.indexOf("!") !== -1) {
     resolve(channel, user, message);
     return;
   }
-
-  client.action(channel, user.username + " написал " + message);
 });
 
-client.on("join", function (channel, username, self) {
-  if (username === "zortan3301") // bot came
-    return;
+// client.on("join", function (channel, username, self) {
+//   if (username === "zortan3301") // bot came
+//     return;
+  
+//   other_bots.forEach(function(nick) {
+//     console.log(username + " : " + nick) // ignore other bots
+//     if (username === nick)
+//       return;
+//   })
 
-  client.action(channel, username + hellos[Math.floor(Math.random() * hellos.length)]);
-});
+//   client.action(channel, username + hellos[Math.floor(Math.random() * hellos.length)]);
+// });
+
+function init_viewers() {
+  var url_string = "https://tmi.twitch.tv/group/user/pray_play/chatters";
+  axios.get(url_string)
+  .then(function (response) {
+    viewers = response['data']['chatters']['viewers'];
+    console.log(JSON.stringify(viewers));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function containsObject(obj, list) {
+  var i;
+  for (i = 0; i < list.length; i++) {
+      if (list[i] === obj) {
+          return true;
+      }
+  }
+
+  return false;
+}
 
 const hellos = [
   ", приветик!!! prayplHello",
   ", дарова prayplHello",
-  " praypHello prayplHello prayplHello",
+  " prayplHello prayplHello prayplHello",
   ", привет prayplHello",
   ", привеееет prayplHello",
   " prayplHello хай",
   " ку prayplHello"
+]
+
+const other_bots = [
+  'moobot',
+  'streamelements'
 ]
