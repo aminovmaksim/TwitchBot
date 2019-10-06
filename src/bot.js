124ab07d8f87f1962b0d92feb282d4088fd3a8e4
Max Aminov <aminovmaksim@gmail.com>
2019-10-06 19:27:37 +0300

    Fix bugs

--- src/bot.js
+++ src/bot.js
@@ -18,13 +18,13 @@ client.on("chat", (channel, user, message, self) => {
 
   if (!isAlreadyViewer(channel, user.username)) {
     proceedNewViewer(channel, user.username);
-    //client.action(channel, user.username + hello[Math.floor(Math.random() * hello.length)]);
+    client.action(channel, user.username + hello[Math.floor(Math.random() * hello.length)]);
   }
 
   message = message.toLowerCase();
 
   if (message.includes("бот") || message.includes("mrdestructoid")) {
-    //client.action(channel, user.username + " сам ты бот MrDestructoid");
+    client.action(channel, user.username + " сам ты бот MrDestructoid");
     return;
   }
 });
