7b670d01d8009a64bbc05755456b4872387b99a2
Max Aminov <aminovmaksim@gmail.com>
2019-10-08 18:42:28 +0300

    Fix bugs

--- src/bot.js
+++ src/bot.js
@@ -72,7 +72,7 @@ function isStreamOnline(channelId) {
   xmlHttp.setRequestHeader("Client-ID", config.client_id);
   xmlHttp.send(null);
 
-  if (xmlHttp.responseText === 'undefined')
+  if (xmlHttp.responseText === "undefined")
     return false;
   var response = JSON.parse(xmlHttp.responseText);
   if (response['data'].length === 0) {
